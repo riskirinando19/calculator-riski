a641b3a8d98f341a07a0d167d1fa47e51411aaa7
riskirinando19 <116501870+riskirinando19@users.noreply.github.com>
2022-10-23 21:58:27 +0700

    Delete k8s/prometheus-grafana directory

--- k8s/prometheus-grafana/prometheus-grafana.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts \
-&& helm repo add stable https://charts.helm.sh/stable \
-&& helm repo update \
-&& helm install prom-grafana prometheus-community/kube-prometheus-stack
